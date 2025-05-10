1e89c497e37e5a4ff138106331e3bca2e6f447fd
Ryann Cláudio <ryanncmm2002@gmail.com>
2025-05-10 13:12:48 -0300

    [FEAT] Configuração rotas header

---
+++ AgroMercadoGerenciador/src/assets/components/header.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Produtos</Link>
+            </li>
+            <li>
+              <Link to="/products/register">Registrar Produto</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
--- AgroMercadoGerenciador/src/main.tsx
+++ AgroMercadoGerenciador/src/main.tsx
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./assets/pages/home/index.tsx";
 import ProductsRegister from "./assets/pages/products-register/products-register.tsx";
 import ProductsView from "./assets/pages/products-view/products-view.tsx";
+import Header from "./assets/components/header.tsx";
+import Footer from "./assets/components/footer.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/products" element={<ProductsView />} />
       <Route path="/products/register" element={<ProductsRegister />} />
     </Routes>
+    <Footer/>
   </BrowserRouter>
 );
